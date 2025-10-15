02f3a2006e2ecbf7b8cf8d598f675e61d5405870
thanh2122005 <thanh2122005@gmail.com>
2025-10-15 21:34:49 +0700

    capnhatdanhmucmodel

--- Model/DanhMucModel.cs
+++ Model/DanhMucModel.cs
@@ -8,5 +8,13 @@ namespace Model
         public string TenDanhMuc { get; set; }
         public int? STT { get; set; }
         public bool TrangThai { get; set; }
+        DanhMucModel()
+        {
+            MaDanhMuc = 0;
+            MaDanhMucCha = null;
+            TenDanhMuc = string.Empty;
+            STT = null;
+            TrangThai = true;
+        }
     }
 }
